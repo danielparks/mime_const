e34c945e57a528b17b8ed4f5e336e3ad722012c9
Daniel Parks <dp+git@oxidized.org>
2025-09-20 18:22:42 -0700

    Add benchmark for storing media type in various ways.
    This reverts commit 5208348569aecb800ce474b9dc8b58f9f3518dd6.

---
+++ benches/general.rs
@@ -0,0 +1,172 @@
+//! Determine relative performance of various ways of constructing a media type.
+
+#![allow(
+    clippy::incompatible_msrv,
+    clippy::missing_docs_in_private_items,
+    missing_docs
+)]
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use std::hint::black_box;
+use std::time::Duration;
+
+struct IndexMime<T>
+where
+    T: Into<usize> + From<u8> + Copy,
+{
+    source: &'static str,
+    slash: T,
+    plus: Option<T>,
+    end: T,
+}
+
+trait Mime {
+    fn type_(&self) -> &'static str;
+    fn subtype(&self) -> &'static str;
+    fn suffix(&self) -> Option<&'static str>;
+}
+
+impl<T> Mime for IndexMime<T>
+where
+    T: Into<usize> + From<u8> + Copy,
+{
+    fn type_(&self) -> &'static str {
+        &self.source[0..self.slash.into()]
+    }
+
+    fn subtype(&self) -> &'static str {
+        &self.source[self.slash.into() + 1..self.end.into()]
+    }
+
+    fn suffix(&self) -> Option<&'static str> {
+        self.plus
+            .map(|plus| &self.source[plus.into() + 1..self.end.into()])
+    }
+}
+
+struct StrMime {
+    _source: &'static str,
+    type_: &'static str,
+    subtype: &'static str,
+    suffix: Option<&'static str>,
+}
+
+impl Mime for StrMime {
+    fn type_(&self) -> &'static str {
+        self.type_
+    }
+
+    fn subtype(&self) -> &'static str {
+        self.subtype
+    }
+
+    fn suffix(&self) -> Option<&'static str> {
+        self.suffix
+    }
+}
+
+const INDEX_MIME_TEXT_U8: IndexMime<u8> = IndexMime::<u8> {
+    source: "text/plain; charset=utf-8",
+    slash: 4,
+    plus: None,
+    end: 10,
+};
+
+const INDEX_MIME_TEXT_U16: IndexMime<u16> = IndexMime::<u16> {
+    source: "text/plain; charset=utf-8",
+    slash: 4,
+    plus: None,
+    end: 10,
+};
+
+const INDEX_MIME_TEXT_USIZE: IndexMime<usize> = IndexMime::<usize> {
+    source: "text/plain; charset=utf-8",
+    slash: 4,
+    plus: None,
+    end: 10,
+};
+
+const INDEX_MIME_SVG_U8: IndexMime<u8> = IndexMime::<u8> {
+    source: "image/svg+xml",
+    slash: 5,
+    plus: Some(9),
+    end: 13,
+};
+
+const INDEX_MIME_SVG_U16: IndexMime<u16> = IndexMime::<u16> {
+    source: "image/svg+xml",
+    slash: 5,
+    plus: Some(9),
+    end: 13,
+};
+
+const INDEX_MIME_SVG_USIZE: IndexMime<usize> = IndexMime::<usize> {
+    source: "image/svg+xml",
+    slash: 5,
+    plus: Some(9),
+    end: 13,
+};
+
+const STR_MIME_TEXT: StrMime = StrMime {
+    _source: "text/plain; charset=utf-8",
+    type_: "text",
+    subtype: "plain",
+    suffix: None,
+};
+
+const STR_MIME_SVG: StrMime = StrMime {
+    _source: "image/svg+xml",
+    type_: "image",
+    subtype: "svg+xml",
+    suffix: Some("xml"),
+};
+
+fn test_mime<M: Mime>(text: M, svg: M) {
+    assert_eq!(black_box(text.type_()), "text");
+    assert_eq!(black_box(text.subtype()), "plain");
+    assert_eq!(black_box(text.suffix()), None);
+
+    assert_eq!(black_box(svg.type_()), "image");
+    assert_eq!(black_box(svg.subtype()), "svg+xml");
+    assert_eq!(black_box(svg.suffix()), Some("xml"));
+}
+
+fn benchmarks(c: &mut Criterion) {
+    let mut group = c.benchmark_group("mime_type");
+    group
+        .noise_threshold(0.10)
+        .significance_level(0.01)
+        .confidence_level(0.99)
+        .sample_size(300)
+        .warm_up_time(Duration::from_millis(100))
+        .measurement_time(Duration::from_secs(1));
+
+    group.bench_function(
+        BenchmarkId::new("u8", size_of_val(&INDEX_MIME_TEXT_U8)),
+        //"u8",
+        |b| b.iter(|| test_mime(INDEX_MIME_TEXT_U8, INDEX_MIME_SVG_U8)),
+    );
+    group.bench_function(
+        BenchmarkId::new("u16", size_of_val(&INDEX_MIME_TEXT_U16)),
+        |b| b.iter(|| test_mime(INDEX_MIME_TEXT_U16, INDEX_MIME_SVG_U16)),
+    );
+    group.bench_function(
+        BenchmarkId::new("usize", size_of_val(&INDEX_MIME_TEXT_USIZE)),
+        |b| b.iter(|| test_mime(INDEX_MIME_TEXT_USIZE, INDEX_MIME_SVG_USIZE)),
+    );
+    group.bench_function(
+        BenchmarkId::new("str", size_of_val(&STR_MIME_TEXT)),
+        |b| b.iter(|| test_mime(STR_MIME_TEXT, STR_MIME_SVG)),
+    );
+    group.bench_function(BenchmarkId::new("control", 0), |b| {
+        b.iter(|| {
+            test_mime(STR_MIME_TEXT, STR_MIME_SVG);
+            test_mime(INDEX_MIME_TEXT_USIZE, INDEX_MIME_SVG_USIZE);
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(general_group, benchmarks);
+criterion_main!(general_group);
