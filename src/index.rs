8f2f4ea3958c4397d22fa82967deb7f86534dd21
Daniel Parks <dp+git@oxidized.org>
2025-09-21 21:27:36 -0700

    First pass at getting information out of `index::Mime`.

--- src/index.rs
+++ src/index.rs
@@ -1,5 +1,8 @@
 //! MIME/media type stored as a slice with indices to its parts.
 
+use crate::rfc7231::unquote_string;
+use std::borrow::Cow;
+
 // FIXME should implement Eq, PartialEq, Ord, and PartialOrd manually
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Mime<'a> {
@@ -10,11 +13,45 @@ pub struct Mime<'a> {
     pub(crate) parameters: Parameters,
 }
 
+impl<'a> Mime<'a> {
+    pub fn type_(&self) -> &str {
+        &self.source.as_str()[0..self.slash.into()]
+    }
+
+    pub fn subtype(&self) -> &str {
+        &self.source.as_str()[usize::from(self.slash) + 1..self.end.into()]
+    }
+
+    pub fn suffix(&self) -> Option<&str> {
+        self.plus.map(|plus| {
+            &self.source.as_str()[usize::from(plus) + 1..self.end.into()]
+        })
+    }
+
+    pub fn parameters(&'a self) -> ParameterIter<'a> {
+        ParameterIter {
+            source: &self.source,
+            parameters: &self.parameters,
+            index: 0,
+        }
+    }
+}
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub(crate) enum Source<'a> {
     Str(&'a str),
     #[expect(dead_code, reason = "FIXME")]
     Owned(String),
+}
+
+impl<'a> Source<'a> {
+    /// Get the source as a `&str`.
+    pub fn as_str(&'a self) -> &'a str {
+        match self {
+            Self::Str(src) => src,
+            Self::Owned(src) => src.as_str(),
+        }
+    }
 }
 
 /// Parameters in a MIME type.
@@ -50,4 +87,64 @@ pub(crate) struct Parameter {
 
     /// Whether or not the value is a quoted string.
     pub(crate) quoted: bool,
+}
+
+impl Parameter {
+    fn name<'a>(&self, src: &'a str) -> &'a str {
+        &src[self.start.into()..self.equal.into()]
+    }
+
+    fn value<'a>(&self, src: &'a str) -> Cow<'a, str> {
+        if self.quoted {
+            // Strip quotes.
+            unquote_string(
+                &src[usize::from(self.equal) + 2..usize::from(self.end) - 1],
+            )
+        } else {
+            Cow::from(&src[usize::from(self.equal) + 1..self.end.into()])
+        }
+    }
+
+    fn tuple<'a>(&self, src: &'a str) -> (&'a str, Cow<'a, str>) {
+        (self.name(src), self.value(src))
+    }
+}
+
+/// Iterator over parameters.
+pub struct ParameterIter<'a> {
+    source: &'a Source<'a>,
+    parameters: &'a Parameters,
+    index: usize,
+}
+
+impl<'a> Iterator for ParameterIter<'a> {
+    type Item = (&'a str, Cow<'a, str>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.parameters {
+            Parameters::None => None,
+            Parameters::One(one) => {
+                if self.index == 0 {
+                    self.index += 1;
+                    Some(one.tuple(self.source.as_str()))
+                } else {
+                    None
+                }
+            }
+            Parameters::Two(one, two) => {
+                if self.index == 0 {
+                    self.index += 1;
+                    Some(one.tuple(self.source.as_str()))
+                } else if self.index == 1 {
+                    self.index += 1;
+                    Some(two.tuple(self.source.as_str()))
+                } else {
+                    None
+                }
+            }
+            Parameters::Many => {
+                panic!("unimplemented"); // FIXME
+            }
+        }
+    }
 }
